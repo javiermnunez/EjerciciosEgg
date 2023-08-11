79fb6e44e3b2c0bc745cc0f5b8f6c79479358d94
javierNuez <javier.nuez91@gmail.com>
2023-08-11 14:39:14 -0300

    subir

---
+++ Ejercicio011/script.js
@@ -0,0 +1,14 @@
+function tomarEntrada(){
+    let entrada = document.getElementById("entrada");
+    console.log(entrada.value);//aca poner la funcion que toma la frase.
+    traeLargo(entrada.value);
+    console.log((entrada.value).lenght);
+}
+function traeLargo(frase){
+    const arrayFrase = new Array();
+    let largo = "";
+    console.log(frase.lenght);
+    for(let i=0;i<frase.lenght;i++){
+        pass;
+    }
+}
